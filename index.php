d6cc610ab26ddbd981eee0d3adeab02b58b0ee5a
Tomoaki iMac5K-1 <tomoaki.nagahara@gmail.com>
2019-02-13 16:39:12 +0900

    New: index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * unit-markdown:/index.php
+ *
+ * @creation  2019-02-13
+ * @version   1.0
+ * @package   unit-markdown
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+//	...
+include('Markdown.class.php');
+
+//	...
+return new Markdown();
